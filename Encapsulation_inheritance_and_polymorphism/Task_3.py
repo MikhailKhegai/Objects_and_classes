db11aa7d517c5c84b8975929ac8688488e2ce466
Mikhail Khegai <khegai200529@gmail.com>
2024-02-05 16:15:38 +0300

    Init

---
+++ Encapsulation_inheritance_and_polymorphism/Task_3.py
@@ -0,0 +1,78 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def average_grade(self):
+        for i in list(self.grades.values()):
+            return (sum(i) / len(i))
+
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_grade()}\nКурсы в процессе изучения: {", ".join(self.courses_in_progress)}\nЗавершенные курсы: {", ".join(self.finished_courses)}'
+
+    def __lt__(self, other_student):
+        return self.average_grade() < other_student.average_grade()
+
+    def __eq__(self, other_student):
+        return self.average_grade() == other_student.average_grade()
+
+    def __le__(self, other_student):
+        return self.average_grade() <= other_student.average_grade()
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def average_grade(self):
+        for i in list(self.grades.values()):
+            return (sum(i) / len(i))
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_grade()}'
+
+    def __lt__(self, other_lecturer):
+        return self.average_grade() < other_lecturer.average_grade()
+
+    def __eq__(self, other_lecturer):
+        return self.average_grade() == other_lecturer.average_grade()
+
+    def __le__(self, other_lecturer):
+        return self.average_grade() <= other_lecturer.average_grade()
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
